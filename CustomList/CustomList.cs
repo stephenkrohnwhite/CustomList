502fdd49e3f574c717b7a82083a79c00051c72fa
Steve White <stephenkrohnwhite@gmail.com>
2018-06-05 20:50:24 -0500

    added project structure and add test and indexer

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -8,5 +8,90 @@ namespace CustomList
 {
     public class CustomList<T>
     {
+        private T[] array;
+        private int capacity;
+        public T[] Array
+        {
+            get
+            {
+                return array;
+            }
+            set
+            {
+                array = value;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+        T value;
+        // Method that adds objects to instance of class
+        // Method that remove objects to instance of class
+        // Method that allows iteration
+        // Method for overriding ToString - converts to string
+        // Method for overloading + operator
+        // Method for overLoading - operator
+        // Property for count
+        // Method for Zip
+
+        public CustomList(int capacityValue)
+        {
+           Capacity = capacityValue;
+
+        }
+        
+        public T this[int i]
+        {
+            get
+            {
+                return Array[i];
+            }
+            set
+            {
+                Array[i] = value;
+            }
+        }
+        
+       
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+        public void Iterate()
+        {
+
+        }
+        public void ConvertToString()
+        {
+
+        }
+        public void OverloadPlus()
+        {
+
+        }
+        public void OverloadMinus()
+        {
+
+        }
+        public void Count()
+        {
+
+        }
+        public void Zip()
+        {
+
+        }
     }
 }
--- UnitTestProject3/CustomListTest.cs
+++ UnitTestProject3/CustomListTest.cs
@@ -7,6 +7,7 @@ namespace UnitTestProject3
     [TestClass]
     public class CustomListTest
     {
+        // testMethod that tests indexer
         // testMethod that adds objects to instance of class
         // testMethod that remove objects to instance of class
         // testMethod that allows iteration
@@ -15,10 +16,34 @@ namespace UnitTestProject3
         // testMethod for overLoading - operator
         // testMethod for count
         // testMethod for Zip
-
+        
         [TestMethod]
-        public void TestMethod1()
+        public void Add_Generic_To_Custom_List()
         {
+            // arrange
+            CustomList<int> num = new CustomList<int>(5);
+            int expectedResult = 2;
+
+            // act
+            num.Add(expectedResult);
+
+            // assert
+            Assert.AreEqual(expectedResult, num[0]);
+        }
+        [TestMethod]
+        public void Indexer_References_Array_Index()
+        {
+            // arrange
+            CustomList<int> integerList = new CustomList<int>(5);
+            int expectedResult = 21;
+
+            // act
+            integerList.Add(1);
+            integerList.Add(21);
+            int actualResult = integerList[1];
+
+            // assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
