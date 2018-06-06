d9c2ede8572c2de3d8ccc0f3f2ad2bcae4f266d4
Steve White <stephenkrohnwhite@gmail.com>
2018-06-05 19:44:30 -0500

    adding initial commit

---
+++ UnitTestProject3/CustomListTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CustomList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        // testMethod that adds objects to instance of class
+        // testMethod that remove objects to instance of class
+        // testMethod that allows iteration
+        // testMethod for overriding ToString - converts to string
+        // testMethod for overloading + operator
+        // testMethod for overLoading - operator
+        // testMethod for count
+        // testMethod for Zip
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
